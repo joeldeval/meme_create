fff7346e678e3ced2b18b4e54f18209868cc13cb
Joel Valdivia <jooelvaldivi@gmail.com>
2016-11-29 22:34:51 -0600

    primer commit con funciones básicas

---
+++ footer.php
@@ -0,0 +1,21 @@
+		<div class="container-fluid footer-container">
+		    <footer>
+		     	<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<p>&copy; Joel Valdivia.  <a href="http://www.jvadivia.org">jvaldivia.org</a></p>
+					</div>
+		     		<div class="col-md-12 footer-icons">
+						<!-- <a href="https://twitter.com/joeldeval" target="_blank" ><img src="http://jvaldivia.org/inicio/wp-content/uploads/2016/09/twitter-circle-logo.png" id="footer-icon-twitter" /></a> -->
+						
+						<a href="https://github.com/joeldeval" target="_blank"><img src="http://jvaldivia.org/inicio/wp-content/uploads/2016/11/github-logo-3.png" id="footer-icon-github" />
+					</div>
+		     	</div>
+			</footer>
+		</div>
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</body>
+</html>
